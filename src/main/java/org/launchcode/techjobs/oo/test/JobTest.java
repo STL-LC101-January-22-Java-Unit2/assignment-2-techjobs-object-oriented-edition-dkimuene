7dc505fdc743118a85e7a69ff429099a39764e3b
Deodat Kimuene <dkimuene1@gmail.com>
2022-04-13 15:54:03 -0500

    Use Unit Testing to Verify Parts of the Job Class (Empty Constructor, Full Constructor, equals Method)

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,53 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    Job job1;
+    Job job2;
+    Job job3;
+    Job job4;
 
+
+    @Before
+    public void createJobObjects(){
+        job1 = new Job();
+        job2 = new Job();
+        job3 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        job4 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testSettingJobID(){
+        assertFalse(job1.getId()== job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertEquals(job3.getName(),"Product tester");
+        assertEquals(job3.getEmployer().getValue(),"ACME");
+        assertEquals(job3.getLocation().getValue(),"Desert");
+        assertEquals(job3.getPositionType().getValue(),"Quality control");
+        assertEquals(job3.getCoreCompetency().getValue(),"Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        assertFalse(job3.equals(job4));
+    }
 }
